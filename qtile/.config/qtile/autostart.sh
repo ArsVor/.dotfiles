ce39699eebb62bd1f38e067b4257314895810c94
ArsVor <artevor1207@gmail.com>
2025-03-22 22:37:52 +0200

    qtile/.config/qtile/autostart.sh: xset s/dpms set to 0 (all)

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -3,13 +3,9 @@
 #source "$HOME/.config/xsecurelock/xsecurelockrc"
 picom --config ~/.config/picom/picom.conf &
 xdotool key Num_Lock &
+xset s 0 0
+xset dpms 0 0 0
 dunst &
 locker &
 ~/.config/qtile/src/load_worckflow.sh
 systemctl --user restart libinput-gestures.service
-
-xset s off &
-# # auto screenlock
-# xset s 600 5
-# xset dpms 600 600 600
-# xss-lock -n /usr/lib/xsecurelock/dimmer -l -- xsecurelock &
