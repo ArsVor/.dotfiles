ff38e01ac4f8cf9f04e1528c6d3cb90d9553fa9f
ArsVor <artevor1207@gmail.com>
2026-04-27 18:13:58 +0300

    scr/qutile_update.sh - modified for niri

--- qtile/.config/qtile/src/qtile_update.sh
+++ qtile/.config/qtile/src/qtile_update.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 
-kitty -e pikaur -Syu && qtile cmd-obj -o widget checkupdates -f force_update
+# kitty -e pikaur -Syu && qtile cmd-obj -o widget checkupdates -f force_update
+kitty -e pikaur -Syu && dms ipc call systemupdater updatestatus
